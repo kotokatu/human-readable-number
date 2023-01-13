ea0a59f5fa716a2b792ff1aee9b9d19f11333aaa
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-01-13 18:13:11 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function toReadable (number) {
-  
+    let numbers = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      10: 'ten',
+      11: 'eleven',
+      12: 'twelve',
+      13: 'thirteen',
+      14: 'fourteen',
+      15: 'fifteen',
+      16: 'sixteen',
+      17: 'seventeen',
+      18: 'eighteen',
+      19: 'nineteen',
+      20: 'twenty',
+      30: 'thirty',
+      40: 'forty',
+      50: 'fifty',
+      60: 'sixty',
+      70: 'seventy',
+      80: 'eighty',
+      90: 'ninety'}
+    let res = '';
+    if (!number) {
+      return 'zero';
+    }
+    let x = Math.floor(number / 100);
+    if (x) {
+      res += numbers[x] + ' hundred ';
+      number = number % 100;
+    }
+    if (numbers[number]) {
+      res += numbers[number];
+    } else if (number) {
+      x = number % 10;
+      res += numbers[number - x] + ' ';
+      res += numbers[x];
+    }
+    return res.trim();
 }
